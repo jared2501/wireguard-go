bd634ffe2ded086b9062994802049be570387f75
David Anderson <danderson@tailscale.com>
2020-03-25 11:55:53 -0700

    device: fix data race accessing persistentKeepaliveInterval.
    Signed-off-by: David Anderson <danderson@tailscale.com>

--- device/device.go
+++ device/device.go
@@ -160,7 +160,11 @@ func deviceUpdateState(device *Device) {
 		device.peers.RLock()
 		for _, peer := range device.peers.keyMap {
 			peer.Start()
-			if peer.persistentKeepaliveInterval > 0 {
+			peer.RLock()
+			keepalive := peer.persistentKeepaliveInterval
+			peer.RUnlock()
+
+			if keepalive > 0 {
 				peer.SendKeepalive()
 			}
 		}
